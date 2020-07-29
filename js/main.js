79ef63824f3059ab980d6d793f1e5459fc109be1
Octavio Ibanez <octaviogd@live.com.mx>
2020-07-29 17:56:45 -0500

    All submit buttons validated

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
 import { Events } from './Events.js';
 
-$(".form-meet").submit(function(event) {
-  event.preventDefault();
-  let fields = $(this).serializeArray();
-  console.log(fields);
-  if (fields[0].value == "") {
-    const error = new Events();
-    error.errorAlert("Esta es una prueba");
-  }
-  else if (fields[1].value == "") {
-    alert("-Vacio");
+
+/* ============================================================
+ General
+ ============================================================ */
+
+ /*All submits button disabled before validating*/
+$('input[type="submit"]').attr('disabled', true).css('opacity', '50%');
+
+/*Event for validating and set submits buttons enabled*/
+$(".form div input").keyup(function(event) {
+  let validaCampos = $('.form').serializeArray();
+  let counter = 0;
+
+  validaCampos.forEach((item, i) => {
+    if (item.value !== "") {
+      counter++;
+    }
+  });
+
+  if (counter === validaCampos.length) {
+    $(this).parent().parent().find('.btn').css({'transition':'all .3s ease', 'opacity':'100%'}).attr('disabled', false);
   }
   else {
-    console.log("lleno");
+    $(this).parent().parent().find('.btn').css({'transition':'all .3s ease', 'opacity':'50%'}).attr('disabled', true);
   }
+});
+
+
+
+/* ============================================================
+ register.html
+ ============================================================ */
+/*Validating fields*/
+$(".form-step-1").submit(function(event) {
+  event.preventDefault();
+
+  let fields = $(this).serializeArray();
+  fields.forEach((item, i) => {
+    console.log(item);
+  });
+
 });
