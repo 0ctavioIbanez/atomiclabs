5849f1f3864a39d14262d10cfa8eb772daf1dd51
Octavio Ibanez <octaviogd@live.com.mx>
2020-07-30 22:54:29 -0500

    Some code updates

--- js/main.js
+++ js/main.js
@@ -6,34 +6,34 @@ import { Valida } from './Valida.js';
  General
  ============================================================ */
 
- /*All submits button disabled before validating*/
-$('input[type="submit"]').attr('disabled', true).css('opacity', '50%');
-
-/*Event for validating and set submits buttons enabled*/
-$(".form div input").keyup(function(event) {
-  let validaCampos = $('.form').serializeArray();
-  let counter = 0;
-
-  validaCampos.forEach((item, i) => {
-    if (item.value !== "") {
-      counter++;
-    }
-  });
-
-  if (counter === validaCampos.length) {
-    $(this).parent().parent().find('.btn').css({'transition':'all .3s ease', 'opacity':'100%'}).attr('disabled', false);
-  }
-  else {
-    $(this).parent().parent().find('.btn').css({'transition':'all .3s ease', 'opacity':'50%'}).attr('disabled', true);
-  }
-});
 
 
 
 /* ============================================================
  register.html
  ============================================================ */
-/*Validating fields*/
+// Enabling submit buttons
+$(".datos .flex").keyup(function(event) {
+  let fieldToValidate = $("form").serializeArray();
+  let counter = 0;
+
+  fieldToValidate.forEach((item, i) => {
+    if (item.value !== "") {
+      counter++;
+    }
+  });
+  if (counter === fieldToValidate.length) {
+    $("input[type='submit']")
+    .css({'transition':'all .3s ease', 'opacity':'100%'}).attr('disabled', false);
+  }
+  else {
+    $("input[type='submit']")
+    .css({'transition':'all .3s ease', 'opacity':'50%'}).attr('disabled', true);
+  }
+});
+
+
+/*Step 1 form*/
 $(".form-step-1").submit(function(event) {
   event.preventDefault();
   const fields = $(this).serializeArray();
